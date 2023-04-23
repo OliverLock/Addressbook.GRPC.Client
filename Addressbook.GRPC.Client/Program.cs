8d1ad3c56238720e431511a76f2c478aa641356e
Oliver Lock <oliverlock@ntlworld.com>
2023-04-23 19:42:23 +0100

    Add project files.

---
+++ Addressbook.GRPC.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Addressbook.GRPC.Client;
+
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7282");
+
+
+var newContact = new AddContactRequest {
+    FirstName = "John",
+    LastName = "Doe",
+    PhoneNumber = "0123456789",
+    PostCode = "AB12 3CD"
+};
+
+
+var client = new Contacts.ContactsClient(channel);
+var reply = await client.AddContactAsync(newContact);
+
+
+
+
+
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(
+//                  new HelloRequest { Name = "GreeterClient" });
+
+
+
+
+Console.WriteLine($"Added Contact {reply.FirstName} {reply.LastName} with id of {reply.Id}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
